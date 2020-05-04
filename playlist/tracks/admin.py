5c5f94bcbc9d2e28a08646097be755360fd84c5d
brunonunest <brunonunest@gmail.com>
2020-05-04 16:48:00 -0400

    1/login working/all working/before model edit/htmlliststarted/frontendgood/frontendbetter/spotbuttonok/finish1/adminfewchanges

--- playlist/tracks/admin.py
+++ playlist/tracks/admin.py
@@ -2,7 +2,45 @@ from django.contrib import admin
 from .models import Track, Producer, Label
 
 
+class TrackinLine(admin.TabularInline):
+    model = Track.producer.through
+    extra = 1
+
+
+class LabelinLine(admin.TabularInline):
+    model = Label
+    extra = 1
+
+
+class ProducerAdmin(admin.ModelAdmin):
+    inlines = [LabelinLine, TrackinLine]
+    list_display = ('name', 'link',)
+    list_display_links = ('name', 'link',)
+    #list_editable =
+    list_filter = ('name',)
+    search_fields = ('name',)
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'link',
+            )
+        }),
+    )
+
+
+    def combine_name_producer(self, obj):
+        return "{} - {}".format(obj.name, obj.producer)
+
+
+admin.site.register(Producer, ProducerAdmin)
 admin.site.register(Track)
-admin.site.register(Producer)
 admin.site.register(Label)
+
+
+
+
+#admin.site.register(Track)
+#admin.site.register(Producer)
+#admin.site.register(Label)
 
